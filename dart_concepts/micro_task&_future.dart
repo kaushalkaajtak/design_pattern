f5d8c733367a4bb3910cebb99b66055e50abe390
kaushalkaajtak <kaushal.kumar2@aajtak.com>
2024-01-07 20:34:48 +0530

    added microtask and future concept

---
+++ dart_concepts/micro_task&_future.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+void main() async {
+  // Define a regular expression to match content inside <p> tags
+  print("before");
+
+  Future(() {
+    print('in future');
+    loopMillionTimes('from future').then((_) {
+      print("returned");
+    });
+  });
+
+  print("after");
+  scheduleMicrotask(() {
+    print('in microtask');
+    print('micro task');
+    Future(() {
+      loopMillionTimes('from microtask').then((_) {
+        print("returned from microtasl");
+      });
+    });
+  });
+
+  loopMillionTimes('from main').then((_) {
+    print("returned");
+  });
+}
+
+Future<void> loopMillionTimes(String where) async {
+  print(where);
+  for (int i = 0; i < 10000000000; i++) {}
+  print("finished");
+}
